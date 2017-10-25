da0e25b11db152fa717aacc817c2a35079c14b39
rennan123 <rennan.h.oliveira@gmail.com>
2017-10-25 21:43:46 -0200

    configurado e pronto pro deploy

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
-from modelo import criar_tarefa
+from modelo import (criar_tarefa, memdb, listar_tarefas, remover_tarefa,
+                    recuperar_tarefa, Tarefa, editar_tarefa)
 
 app = Flask('Meu app')
 
 
 @app.route('/task', methods=['POST'])
 def criar():
+    memdb.clear()
     # recebe o título e a descrição através do corpo da requisição
     titulo = request.form['titulo']
     descricao = request.form['descricao']
@@ -19,3 +21,55 @@ def criar():
         'descricao': tarefa.descricao,
         'status': tarefa.status,
     }), 201
+
+
+@app.route('/task', methods=['GET'])
+def listar():
+    tarefas = []
+    for tarefa in listar_tarefas():
+        tarefas.append({
+            'id': tarefa.id,
+            'titulo': tarefa.titulo,
+            'status': tarefa.status,
+        })
+    return jsonify(tarefas)
+
+
+@app.route('/task/<int:id_tarefa>', methods=['DELETE'])
+def remover(id_tarefa):
+    try:
+        remover_tarefa(id_tarefa)
+        return '', 204
+    except KeyError:
+        return jsonify({'error': 'task not found'}), 404
+
+
+@app.route('/task/<int:id_tarefa>', methods=['GET'])
+def detalhar(id_tarefa):
+    try:
+        tarefa = recuperar_tarefa(id_tarefa)
+        return jsonify({
+            'id': tarefa.id,
+            'titulo': tarefa.titulo,
+            'descricao': tarefa.descricao,
+            'status': tarefa.status,
+        })
+    except KeyError:
+        return jsonify({'error': 'task not found'}), 404
+
+
+@app.route('/task/<int:id_tarefa>', methods=['PATCH', 'PUT'])
+def editar(id_tarefa):
+    try:
+        t = Tarefa(request.form['titulo'],
+                   request.form['descricao'],
+                   request.form['status'])
+        tarefa = editar_tarefa(id_tarefa, t)
+        return jsonify({
+            'id': tarefa.id,
+            'titulo': tarefa.titulo,
+            'descricao': tarefa.descricao,
+            'status': tarefa.status,
+        })
+    except KeyError:
+        return jsonify({'error': 'task not found'}), 404
--- test_tarefa.py
+++ test_tarefa.py
@@ -1,6 +1,6 @@
 import json
 from main import app
-from modelo import memdb
+from modelo import memdb, Tarefa
 
 
 def test_criar_tarefa():
@@ -23,3 +23,74 @@ def test_erro_ao_criar_tarefa():
     with app.test_client() as c:
         resp = c.post('/task', data={'titulo': 'titulo'})
         assert resp.status_code == 400
+
+
+def test_listar_tarefa_sem_conteudo():
+    memdb.clear()
+    with app.test_client() as c:
+        resp = c.get('/task')
+        data = json.loads(resp.data.decode('utf-8'))
+        assert resp.status_code == 200
+        assert data == []
+
+
+def test_listar_tarefa_com_conteudo():
+    memdb.clear()
+    tarefa1 = Tarefa('titulo 1', 'descrição')
+    memdb[tarefa1.id] = tarefa1
+    with app.test_client() as c:
+        resp = c.get('/task')
+        data = json.loads(resp.data.decode('utf-8'))
+        assert resp.status_code == 200
+        assert len(data) == 1
+        assert 'descricao' not in data[0]
+
+
+def test_remover_tarefa():
+    memdb.clear()
+    tarefa1 = Tarefa('titulo 1', 'descrição')
+    memdb[tarefa1.id] = tarefa1
+    with app.test_client() as c:
+        resp = c.delete('/task/{}'.format(tarefa1.id))
+        assert resp.data == b''
+        assert resp.status_code == 204
+
+
+def test_erro_ao_remover_tarefa():
+    memdb.clear()
+    with app.test_client() as c:
+        resp = c.delete('/task/42')
+        assert resp.status_code == 404
+
+
+def test_detalhando_tarefa():
+    memdb.clear()
+    tarefa = Tarefa('titulo', 'descrição')
+    memdb[tarefa.id] = tarefa
+    with app.test_client() as c:
+        resp = c.get('/task/{}'.format(tarefa.id))
+        assert resp.status_code == 200
+        data = json.loads(resp.data.decode('utf-8'))
+        assert data['titulo'] == 'titulo'
+        assert data['descricao'] == 'descrição'
+
+
+def test_erro_ao_detalhar_tarefa():
+    memdb.clear()
+    with app.test_client() as c:
+        resp = c.get('/task/42')
+        assert resp.status_code == 404
+
+
+def test_entregando_tarefas():
+    memdb.clear()
+    tarefa = Tarefa('titulo', 'descrição')
+    memdb[tarefa.id] = tarefa
+    with app.test_client() as c:
+        resp = c.put('/task/{}'.format(tarefa.id), data={
+            'titulo': tarefa.titulo,
+            'descricao': tarefa.descricao,
+            'status': True})
+        data = json.loads(resp.data.decode('utf-8'))
+        assert resp.status_code == 200
+        assert data['status'] == 'True'
